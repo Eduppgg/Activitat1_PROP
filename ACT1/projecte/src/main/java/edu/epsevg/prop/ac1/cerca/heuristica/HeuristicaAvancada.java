c6a16c336a1d232d7bbd1fcadffeb376ad1c2e46
AdriaSL <adria.serralta@estudiantat.upc.edu>
2025-11-13 11:28:52 +0100

    Update HeuristicaAvancada.java

--- ACT1/projecte/src/main/java/edu/epsevg/prop/ac1/cerca/heuristica/HeuristicaAvancada.java
+++ ACT1/projecte/src/main/java/edu/epsevg/prop/ac1/cerca/heuristica/HeuristicaAvancada.java
@@ -1,26 +1,26 @@
 package edu.epsevg.prop.ac1.cerca.heuristica;
 
+import java.util.List;
+
 import edu.epsevg.prop.ac1.model.Mapa;
 import edu.epsevg.prop.ac1.model.Posicio;
 
-import java.util.*;
-
-/**
- * Heuristica avançada: Al vostre gust ;-)
- */
 public class HeuristicaAvancada implements Heuristica {
-    
+
     @Override
     public int h(Mapa estat) {
-        
-        
-        //@TODO: reemplaceu tot el codi per la vostra heurística.
-        
-        if(estat.esMeta()) 
-            return 0;
-        else 
-            return 1;
-    }
+        if (estat.esMeta()) return 0;
 
-     
+        List<Posicio> agents = estat.getAgents();
+        Posicio sortida = estat.getSortida();
+
+        int millor = Integer.MAX_VALUE;
+
+        for (Posicio ag : agents) {
+            int d = Math.abs(ag.x - sortida.x) + Math.abs(ag.y - sortida.y);
+            if (d < millor) millor = d;
+        }
+
+        return millor;
+    }
 }
